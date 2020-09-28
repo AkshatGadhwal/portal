528bd94a86534cd46f2f4b40a0b9146d7169e4a5
inductor69 <adityakiitdelhi@gmail.com>
2020-09-28 14:58:14 +0530

    Added Backend

---
+++ Backend/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDLo9Cc9zmRW6_iMI1dRULMbMc9QGiPCIg",
+  authDomain: "peer-connect-16fe4.firebaseapp.com",
+  databaseURL: "https://peer-connect-16fe4.firebaseio.com",
+  projectId: "peer-connect-16fe4",
+  storageBucket: "peer-connect-16fe4.appspot.com",
+  messagingSenderId: "796469026635",
+  appId: "1:796469026635:web:c9afde34286e208218d2ce",
+  measurementId: "G-FTB36QTNJ9",
+};
